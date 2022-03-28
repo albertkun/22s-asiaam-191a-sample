c856aec9ef03959e64759ebe870c7784f529f5d2
albertkun <albertk@gmx.com>
2022-03-27 21:28:37 -0700

    feat: added .js and .css for lab1

---
+++ week1/js/init.js
@@ -0,0 +1,17 @@
+let map = L.map('map').setView([34.0709, -118.444], 15);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+let marker = L.marker([34.0709, -118.444]).addTo(map)
+		.bindPopup('Math Sciences 4328 aka the Technology Sandbox<br> is the lab where I will hold office hourse!')
+		.openPopup();
+
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties) {
+        layer.bindPopup("<h3>"+feature.properties.name+"<br>Pop_density: "+feature.properties.POP_DENSIT+"</h1>");
+		console.log(feature.properties)
+    }
+}
